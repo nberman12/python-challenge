001400958820b739c120696f2e7561e521a761aa
Nate Berman <nateberman12@gmail.com>
2020-03-07 14:06:17 -0500

    Updated scripts and Outputs

---
+++ PyBank/Main.py
@@ -0,0 +1,64 @@
+import os
+import csv
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csvpath = os.path.join(dir_path,"03-Python_homework_assignment_PyBank_Resources_budget_data.csv")
+
+output_file=os.path.join(dir_path,"Final_PyBank.txt")
+
+date=[]
+profit_loss=[]
+net_change=[]
+prev_row=[]
+greatest=0
+lowest=0
+
+with open(csvpath) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    csv_header=next(csvreader)
+    prev_row=next(csvreader)
+    
+    for row in csvreader:
+        #Months
+        date.append(row[0])
+        months=int(len(date))+1
+        #Total
+        profit_loss.append(int(row[1]))
+        total=sum(profit_loss)
+        #Average Change
+        net_change.append(int(row[1])-int(prev_row[1]))
+        average_change=round(sum(net_change)/len(net_change),2)
+        
+        #Greatest Increase
+        greatest_increase=max(net_change)
+        greatest=net_change.index(greatest_increase)
+        month_increase=date[greatest]
+
+        #Greatest Loss
+        greatest_loss=min(net_change)
+        lowest=net_change.index(greatest_loss)
+        month_loss=date[lowest]
+
+        prev_row=row
+
+print(f"Financial Analysis")
+print("------------------------------------")
+print(f"Total Months: {months}")        
+print(f"Total: $ {total}")
+print(f"Average Change: $ {average_change}")
+print(f"Greatest Increase in Profits: {month_increase} ${greatest_increase} ")
+print(f"Greatest Decrease in Profits: {month_loss} ${greatest_loss}")  
+
+with open(output_file, "w") as txtfile:
+    txtfile.write('Financial Analysis')
+    txtfile.write('\n----------------------------------------')
+    txtfile.write(f'\nTotal Months: {months}')
+    txtfile.write(f'\nTotal: ${total}')
+    txtfile.write(f'\nAverage Change: ${average_change}')
+    txtfile.write(f'\nGreatest Increase in Profits: {month_increase} ${greatest_increase}')
+    txtfile.write(f'\nGreatest Decrease in Profits:: {month_loss} ${greatest_loss}')
+
+        
+
+
+
---
+++ PyPoll/Main.py
@@ -0,0 +1,61 @@
+import csv
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csvpath = os.path.join(dir_path,"03-Python_homework_assignment_PyPoll_Resources_election_data.csv")
+
+output_file=os.path.join(dir_path,"Final_PyPoll.txt")
+
+total=0
+candidate=[]
+unique_candidate=[]
+vote_count=[]
+vote_percent=[]
+
+
+with open(csvpath) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    csv_header=next(csvreader)
+
+    for row in csvreader:
+        #Total votes
+        total=total+1
+
+        #Candidate count
+        candidate.append(row[2])
+   
+    #Loop through, find the count for each candidate 
+    #and calc % of votes for each candidate
+    for c in set(candidate):
+        unique_candidate.append(c)
+        v=candidate.count(c)
+        vote_count.append(v)
+        p=round((v/total)*100,2)
+        vote_percent.append(p)
+    
+    #Identify the winner based on vote count adn index name based on count
+    winner_count=max(vote_count)
+    winner=unique_candidate[vote_count.index(winner_count)]
+
+#Print results in terminal
+print('Election Results')
+print('--------------------------------')
+print(f'Total Votes: {total}')
+print('--------------------------------')
+for i in range(len(unique_candidate)):
+    print(f'{unique_candidate[i]}: {vote_percent[i]}% {vote_count[i]}')
+print('--------------------------------')
+print(f'Winner: {winner}')
+print('--------------------------------')
+
+#Write results to text file
+with open(output_file, "w") as txtfile:
+    txtfile.write('Election Results')
+    txtfile.write('\n------------------------------------')
+    txtfile.write(f'\nTotal Votes: {total}')
+    txtfile.write('\n------------------------------------')
+    for i in range (len(unique_candidate)):
+        txtfile.write(f'\n{unique_candidate[i]}: {vote_percent[i]}% {vote_count[i]}')
+    txtfile.write('\n------------------------------------')
+    txtfile.write(f'\nWinner: {winner}')
+    txtfile.write('\n------------------------------------')
